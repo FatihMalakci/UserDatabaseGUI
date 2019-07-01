8ef828ecab98ab7c6975514881e9e58d12dc8455
FatihMalakci <fatihmalakci@outlook.com>
2019-07-01 06:24:30 +0300

    database wip

--- database.py
+++ database.py
@@ -1,3 +1,18 @@
 import sqlite3
 
 conn = sqlite3.connect('users.db')
+c = conn.cursor()
+
+
+#Creating our table.
+c.execute('''CREATE TABLE users
+             (name,sirname,age,phone number)''')
+
+# Inserting an example user.
+c.execute("INSERT INTO users VALUES('Fatih','Malakçı','24','5419768923')")
+
+#Saving
+conn.commit()
+
+#Closing
+conn.close()
